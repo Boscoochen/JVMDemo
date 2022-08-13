0fa2bb4fed1c83adf57be19bfda89f4634e15813
Boscoochen <boscochen203@gmail.com>
2022-08-13 14:18:55 -0500

    updates jvm code

---
+++ src/com/atguigu/java/chapter04/PCRegisterTest.java
@@ -0,0 +1,16 @@
+package com.atguigu.java.chapter04;
+
+public class PCRegisterTest {
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 20;
+        int k = i + j;
+
+        String s = "abc";
+        System.out.println(i);
+        System.out.println(k);
+
+
+        // javap -v PCRegisterTest.class
+    }
+}
