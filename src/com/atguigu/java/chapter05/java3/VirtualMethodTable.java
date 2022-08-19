f545c8044e0e8a812c8f08e76e0bd65bf360183b
Boscoochen <boscochen203@gmail.com>
2022-08-18 22:47:38 -0500

    update jvm code

---
+++ src/com/atguigu/java/chapter05/java3/VirtualMethodTable.java
@@ -0,0 +1,33 @@
+package com.atguigu.java.chapter05.java3;
+
+interface Friendly {
+    void sayHello();
+    void sayGoodbye();
+}
+
+class Dog {
+    public void sayHello() {}
+    public String toString() {
+        return "Dog";
+    }
+}
+
+class Cat implements Friendly {
+    public void eat() {}
+    public void sayHello() {}
+    public void sayGoodbye() {}
+    protected void finalize() {
+
+    }
+    public String toString() {
+        return "Cat";
+    }
+}
+
+class CockerSpaniel extends Dog implements Friendly {
+    public void sayHello() { super.sayHello(); }
+    public void sayGoodbye() {}
+}
+
+public class VirtualMethodTable {
+}
