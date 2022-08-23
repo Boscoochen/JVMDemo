bfd11a9cd7ecea21139d0e8c1092f7b89012479b
Boscoochen <boscochen203@gmail.com>
2022-08-22 20:37:46 -0500

    updates jvm code

---
+++ src/com/atguigu/java/chapter8/java/HeapDemo.java
@@ -0,0 +1,14 @@
+package com.atguigu.java.chapter8.java;
+
+public class HeapDemo {
+    public static void main(String[] args) {
+        System.out.println("start");
+        try {
+            Thread.sleep(1000000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("end...");
+    }
+}
