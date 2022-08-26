a20325cbb4cb32bd851db6c33b1dc7384d5cd0ca
Boscoochen <boscochen203@gmail.com>
2022-08-26 11:34:31 -0500

    add oom test

---
+++ src/com/atguigu/java/chapter8/java/OOMTest.java
@@ -0,0 +1,25 @@
+package com.atguigu.java.chapter8.java;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class OOMTest {
+    public static void main(String[] args) {
+        ArrayList<Picture> list = new ArrayList<Picture>();
+        while(true) {
+            try {
+                Thread.sleep(20);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            list.add(new Picture(new Random().nextInt(1024 * 1024)));
+        }
+    }
+}
+
+class Picture {
+    private byte[] pixels;
+    public Picture(int length) {
+        this.pixels = new byte[length];
+    }
+}
