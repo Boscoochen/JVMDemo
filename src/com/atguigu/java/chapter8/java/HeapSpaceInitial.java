41d64ed1059cf3f7542bf5790190bcaee6be263b
Boscoochen <boscochen203@gmail.com>
2022-08-25 22:18:50 -0500

    updates jvm code

---
+++ src/com/atguigu/java/chapter8/java/HeapSpaceInitial.java
@@ -0,0 +1,21 @@
+package com.atguigu.java.chapter8.java;
+
+public class HeapSpaceInitial {
+    public static void main(String[] args) {
+        long initialMemory = Runtime.getRuntime().totalMemory() / 1024 / 1024;
+
+        long maxMemory = Runtime.getRuntime().maxMemory() / 1024 / 1024;
+
+        System.out.println("-Xms : " + initialMemory + "M");
+        System.out.println("-Xmx : " + maxMemory + "M");
+
+//        System.out.println("系统内存大小为：" + initialMemory * 64.0 / 1024 + "G");
+//        System.out.println("系统内存大小为：" + maxMemory * 4.0 / 1024 + "G");
+
+//        try {
+//            Thread.sleep(1000000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+    }
+}
