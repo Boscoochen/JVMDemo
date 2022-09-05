0063c5bce1322c1f6901b4215215aa577306da8b
Boscoochen <boscochen203@gmail.com>
2022-09-05 13:18:54 -0500

    同步省略

---
+++ src/com/atguigu/java/chapter08/java2/SynchronizedTest.java
@@ -0,0 +1,10 @@
+package com.atguigu.java.chapter08.java2;
+
+public class SynchronizedTest {
+    public void f() {
+        Object hollis = new Object();
+        synchronized (hollis) {
+            System.out.println(hollis);
+        }
+    }
+}
