790e8a0f5e9917dc0a30fd35fda22a63190c4730
Boscoochen <boscochen203@gmail.com>
2022-09-10 18:50:30 -0500

    方法区的内部结构

--- src/com/atguigu/java/chapter09/java/MethodAreaDemo.java
+++ src/com/atguigu/java/chapter09/java/MethodAreaDemo.java
@@ -1,5 +1,9 @@
 package com.atguigu.java.chapter09.java;
 
+/**
+ * jdk8 以后
+ * -XX:MetaspaceSize=100m  -XX:MaxMetaspaceSize=100m
+ */
 public class MethodAreaDemo {
     public static void main(String[] args) {
         System.out.println("start...");
---
+++ src/com/atguigu/java/chapter09/java/MethodAreaTest.java
@@ -0,0 +1,20 @@
+package com.atguigu.java.chapter09.java;
+
+public class MethodAreaTest {
+    public MethodAreaTest() {}
+
+    public static void main(String[] args) {
+        Order order = null;
+        Order.hello();
+        System.out.println(Order.count);
+    }
+}
+
+class Order {
+    public static int count = 1;
+    public static final int number = 2;
+
+    public static void hello() {
+        System.out.println("hello!");
+    }
+}
