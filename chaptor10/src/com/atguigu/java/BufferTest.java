33b490bede22802a8a2dba37ed229d77ab6b5820
Boscoochen <boscochen203@gmail.com>
2022-09-17 21:41:14 -0500

    finish 直接内存

---
+++ chaptor10/src/com/atguigu/java/BufferTest.java
@@ -0,0 +1,21 @@
+package com.atguigu.java;
+
+import java.nio.ByteBuffer;
+import java.util.Scanner;
+
+public class BufferTest {
+    private static final int BUFFER = 1024 * 1024 * 1024;
+
+    public static void main(String[] args) {
+        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(BUFFER);
+        System.out.println("直接内存分配完毕，请求指示！");
+
+        Scanner scanner = new Scanner(System.in);
+        scanner.next();
+
+        System.out.println("直接内存开始释放！");
+        byteBuffer = null;
+        System.gc();
+        scanner.next();
+    }
+}
