f810802435be34554dc83c85ebb3f3165f412665
Boscoochen <boscochen203@gmail.com>
2022-09-22 15:48:18 -0500

    intern的空间效率测试

---
+++ chapter13/src/com/atguigu/java2/StringIntern3.java
@@ -0,0 +1,22 @@
+package com.atguigu.java2;
+
+public class StringIntern3 {
+    static final int MAX_COUNT = 1000 * 10000;
+    static final String[] arr = new String[MAX_COUNT];
+
+    public static void main(String[] args) throws InterruptedException {
+        Integer[] data = new Integer[]{1,2,3,4,5,6,7,8,9,10};
+
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < MAX_COUNT; i++) {
+//            arr[i] = new String(String.valueOf(data[i % data.length]));
+            arr[i] = new String(String.valueOf(data[i % data.length])).intern();
+
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("花费的时间为：" + (end - start));
+
+        Thread.sleep(1000000);
+        System.gc();
+    }
+}
